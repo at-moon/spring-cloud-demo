78bdd55f2131933c098795655e2ec56353c57881
at-moon <894298646@qq.com>
2020-07-02 14:17:23 +0800

    zuul use consul

--- zuul/src/main/java/com/atmoon/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/atmoon/zuul/ZuulApplication.java
@@ -1,10 +1,8 @@
 package com.atmoon.zuul;
 
-import com.atmoon.zuul.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
