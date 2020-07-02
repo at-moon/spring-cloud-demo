a329ca4650b10e79d264f23b045f596b1bf4f59f
at-moon <894298646@qq.com>
2020-07-02 11:37:10 +0800

    consul consumer init

---
+++ consul-consumer/src/main/java/com/atmoon/consulconsumer/ConsulConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.atmoon.consulconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lenovo
+ */
+@SpringBootApplication
+public class ConsulConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulConsumerApplication.class, args);
+    }
+
+}
---
+++ consul-consumer/src/main/java/com/atmoon/consulconsumer/controller/ServiceController.java
@@ -0,0 +1,55 @@
+package com.atmoon.consulconsumer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author : zy
+ */
+@RestController
+public class ServiceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceController.class);
+
+    @Resource
+    private LoadBalancerClient balancerClient;
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 获取所有服务
+     */
+    @RequestMapping("/services")
+    public Object services() {
+        return discoveryClient.getInstances("consul-producer");
+    }
+
+    /**
+     * 从所有服务中选择一个服务（轮询）
+     */
+    @RequestMapping("/discover")
+    public Object discover() {
+        return balancerClient.choose("consul-producer").getUri().toString();
+    }
+
+    @RequestMapping("/call")
+    public String call() {
+        ServiceInstance serviceInstance = balancerClient.choose("consul-producer");
+        logger.info("服务地址: {}", serviceInstance.getUri());
+        logger.info("服务名称: {}", serviceInstance.getServiceId());
+
+        String callServiceResult = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "/hello", String.class);
+        logger.info(callServiceResult);
+        return callServiceResult;
+    }
+
+
+}
