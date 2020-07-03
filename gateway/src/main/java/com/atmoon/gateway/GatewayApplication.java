72f7db245f78b648fca6861cb6834d8b333b10fc
at-moon <894298646@qq.com>
2020-07-03 18:08:46 +0800

    gateway use redis限流

--- gateway/src/main/java/com/atmoon/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/atmoon/gateway/GatewayApplication.java
@@ -2,9 +2,11 @@ package com.atmoon.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 /**
  * @author lenovo
@@ -22,6 +24,11 @@ public class GatewayApplication {
                 .route("path_route", r -> r.path("/about")
                         .uri("http://ityouknow.com"))
                 .build();
+    }
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("name"));
     }
 
 }
